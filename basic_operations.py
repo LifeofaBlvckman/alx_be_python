0dc40e70b0eb50ce7a6d311ca9621d6e0edc6688
LifeofaBlvckman <prettykoke123@gmail.com>
2025-09-21 22:48:32 +0100

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", addition_result)
+print("Subtraction of", number1, "and", number2, "is", subtraction_result)
+print("Multiplication of", number1, "and", number2, "is", multiplication_result)
