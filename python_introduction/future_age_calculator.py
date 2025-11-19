347768fb495b7de91c039d96e231492dc1524445
mac <mac@BLVCKMANs-MacBook-Pro.local>
2025-11-19 01:02:21 +0100

    Add future_age_calculator.py to python_introduction directory

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# Future Age Calculator
+current_age = int(input("How old are you? "))
+
+# Calculate age in 2050 (current year is 2023, so 2050 - 2023 = 27 years)
+future_age = current_age + 27
+
+print("In 2050, you will be", future_age, "years old")
